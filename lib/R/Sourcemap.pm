88f91342d9b6c5bfe4ce84ddb59ecae7605606a9
dart <darviarush@mail.ru>
2017-02-16 16:34:33 +0300

    ag+

---
+++ lib/R/Sourcemap.pm
@@ -0,0 +1,78 @@
+package R::Sourcemap;
+# отображает скомпиленные файлы
+
+use common::sense;
+use R::App;
+
+# конструктор
+sub new {
+	my ($cls) = @_;
+	bless {}, ref $cls || $cls;
+}
+
+
+# генерирует число из integer в VLQ
+my $VLQ_SHIFT = 5;
+my $VLQ_CONTINUATION_BIT = 1 << $VLQ_SHIFT;
+my $VLQ_VALUE_MASK = $VLQ_CONTINUATION_BIT - 1;
+my $BASE64_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+sub _VLQ {
+	my ($value) = @_;
+	
+    my $answer = '';
+    my $signBit = $value < 0 ? 1 : 0;
+    my $valueToEncode = (abs($value) << 1) + $signBit;
+	while ($valueToEncode || !$answer) {
+		my $nextChunk = $valueToEncode & $VLQ_VALUE_MASK;
+		$valueToEncode = $valueToEncode >> $VLQ_SHIFT;
+		if ($valueToEncode) {
+			$nextChunk |= $VLQ_CONTINUATION_BIT;
+		}
+		$answer .= _encodeBase64($nextChunk);
+	}
+	return $answer;
+}
+
+    
+# в base64
+sub _encodeBase64 {
+	my ($value) = @_;
+    substr($BASE64_CHARS, $value, 1) || die "Cannot Base64 encode value: " + $value;
+}
+
+# генерирует mappings для map-файла
+sub mappings {
+	my ($self, $push) = @_;
+	
+	# Номер символа в сгенерированном файле
+	# Исходный файл
+	# Номер строки в исходном файле
+	# Номер символа в исходном файле
+	# Исходное название (если есть)
+	
+	
+	
+	$push->{lineno}
+	$push->{charno}
+	#pack
+	
+	$self
+}
+
+# генерирует map-файл
+sub mapfile {
+	my ($self, $from_path, $to_path) = @_;
+	
+	
+	
+	return {
+		version => 3,
+		file => $to_path,						#	"app.min.js",
+		sourceRoot => "",
+		sources => [$from_path],				# ["foo.js", "bar.js"],
+		names => [],							# ["src", "maps", "are", "fun"],		
+		mappings => $self->{mappings},			# "AAgBC,SAAQ,CAAEA"
+	}
+}
+
+1;
---
+++ lib/R/Syntax/Preprocessor/Au.pm
@@ -0,0 +1,77 @@
+package R::Syntax::Preprocessor::Au;
+# препроцессор шаблонов - преобразует код шаблона в код Серебряны
+
+use common::sense;
+use R::App;
+
+# конструктор
+# file - файл
+# text - код, если не указан - берётся из файла
+# to - выходной файл
+sub new {
+	my $cls = shift;
+	bless { @_ }, ref $cls || $cls;
+}
+
+# морфирует файл шаблона в код Серебряны
+sub morf {
+	my ($self) = @_;
+	
+	my $text = $self->{text} //= $app->file($self->{file})->read;
+	
+	my $echo = ' @ECHO ,= "';
+	my $end = '"; ';
+	
+	my $inline = '", (';
+	my $outline = '), "';
+	
+	my $escape = '" . app.html.escape(';
+	
+	my @ret;
+	my $inherits;
+	
+	while($text =~ m{
+		(?<text> .*? )
+		( <% ((
+			(?<set>		if | elseif | while | for | end | else ) |
+			(?<get>		get ) |
+			(?<raw>		raw ) |
+			(?<inherits>	extends | inherits )
+		) \b)? (?<tail> .*? ) %> | $ )
+	}gxnis) {
+		my $txt = $+{text};
+		$txt =~ s!"!""!g;
+		$txt =~ s!\\!\\\\!g;
+		
+		push @ret, $txt;
+		
+		if(exists $+{set}) {
+			push @ret, $end, $+{set}, $+{tail}, "\n", $echo;
+		}
+		elsif(exists $+{get}) {
+			push @ret, $escape, $+{tail}, $outline;
+		}
+		elsif(exists $+{raw}) {
+			push @ret, $inline, $+{tail}, $outline;
+		}
+		elsif(exists $+{tail}) {
+			if($+{tail} =~ /==|=<|=>|(=)/ && defined $1) {
+				push @ret, $end, $+{tail}, $echo;
+			} else {
+				push @ret, $inline, $+{tail}, $outline;
+			}
+		}
+		elsif(exists $+{inherits}) {
+			$inherits = $+{tail};
+		}
+		else {
+		}
+	}
+	
+	my $name = $inherits? '__undefined__': 'render';
+	my $return = $inherits? '': ' @(LOCAL DELETE "ECHO") join';
+	
+	join "", ($inherits? $inherits: ()), 'SUB __UNDEF__ THEN BLOCK ', $name ,' THEN DO ', $echo, @ret, $end, 'RETURN', $return, " END\nme.render";
+}
+
+1;
